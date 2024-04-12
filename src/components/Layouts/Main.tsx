eb338461b35c9e273c8411dc268fbd83aca61a7d
Cyclops789 <70551889+Cyclops789@users.noreply.github.com>
2024-04-12 00:46:53 +0000

    Updates

--- src/components/Layouts/Main.tsx
+++ src/components/Layouts/Main.tsx
@@ -15,7 +15,7 @@ export default function Layout({
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="initial-scale=2.0, width=device-width" />
       </Head>
       {children}
     </div>
